5da7dfefa230a817b3330629a5e4c035dec20f64
jkuusto <jukka.kuusto@gmail.com>
2024-11-27 10:33:39 +0200

    Refactoring

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -16,3 +16,43 @@ class Sovelluslogiikka:
 
     def arvo(self):
         return self._arvo
+
+class Summa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        try:
+            arvo = int(self._lue_syote())
+            self._sovelluslogiikka.plus(arvo)
+        except ValueError:
+            pass
+
+class Erotus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        try:
+            arvo = int(self._lue_syote())
+            self._sovelluslogiikka.miinus(arvo)
+        except ValueError:
+            pass
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+
+class Kumoa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        pass
