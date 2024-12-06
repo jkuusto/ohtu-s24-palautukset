f21b42a1a6f411fb5ecc2cd4f62db494ec7e7ca1
jkuusto <jukka.kuusto@gmail.com>
2024-12-06 12:48:22 +0200

    Add QueryBuilder class

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,7 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
+from matchers import (And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or,
+                      QueryBuilder)
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
@@ -24,13 +25,23 @@ def main():
         HasAtLeast(45, "goals"),
         HasAtLeast(70, "assists")
     )'''
-    matcher = And(
+    '''matcher = And(
         HasAtLeast(70, "points"),
         Or(
             PlaysIn("NYR"),
             PlaysIn("FLA"),
             PlaysIn("BOS")
         )
+    )'''
+    query = QueryBuilder()
+    #matcher = query.build()
+    #matcher = query.plays_in("NYR").build()
+    matcher = (
+        query
+        .plays_in("NYR")
+        .has_at_least(10, "goals")
+        .has_fewer_than(20, "goals")
+        .build()
     )
 
     for player in stats.matches(matcher):
